723973e36d43d3e5af501a7eb29f733ddaf12e98
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-03-08 00:10:10 +0530

    Installed Libraries

---
+++ AutoComplete/dist/index.c36f364e.js
@@ -0,0 +1,9 @@
+const Number1 = document.querySelector("#number1");
+const output = document.querySelector("#input1");
+//console.log(Number1.value);
+Number1.innerHTML = "Hemanth!";
+console.log(233);
+const ans = Number1.innerText;
+console.log(ans);
+
+//# sourceMappingURL=index.c36f364e.js.map
---
+++ AutoComplete/index.js
@@ -0,0 +1,7 @@
+const Number1 = document.querySelector("#number1");
+const output = document.querySelector("#input1");
+//console.log(Number1.value);
+Number1.innerHTML = "Hemanth!";
+console.log(233);
+const ans = Number1.innerText;
+console.log(ans);
